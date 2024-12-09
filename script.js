10d97edb5d53ebdcaea0dcff2f5c66c8afed9d1d
umarawais84 <99414210+umarawais84@users.noreply.github.com>
2024-12-09 01:22:15 +0000

    Added images ; Done

--- script.js
+++ script.js
@@ -21,32 +21,48 @@ function shakeMagic8Ball() {
 
     // Responses and images
     let eightBall = "";
-    let imageChoice = "https://via.placeholder.com/300"; 
+    let imageChoice = ""; 
 
     switch (randomNumber) {
         case 0:
             eightBall = "It is certain.";
+                  imageChoice = "imgs/1.avif";
+
             break;
         case 1:
             eightBall = "It is decidedly so.";
+                  imageChoice = "imgs/2.jpg";
+
             break;
         case 2:
             eightBall = "Reply hazy, try again.";
+                  imageChoice = "imgs/3.webp";
+
             break;
         case 3:
             eightBall = "Cannot predict now.";
+                  imageChoice = "imgs/4.webp";
+
             break;
         case 4:
             eightBall = "Do not count on it.";
+            imageChoice = "imgs/5.png";
             break;
+
         case 5:
             eightBall = "My sources say no.";
+                  imageChoice = "imgs/6.png";
+
             break;
         case 6:
             eightBall = "Outlook not so good.";
+                  imageChoice = "imgs/7.jpg";
+
             break;
         case 7:
             eightBall = "Signs point to yes.";
+                  imageChoice = "imgs/8.jpg";
+
             break;
     }
 
